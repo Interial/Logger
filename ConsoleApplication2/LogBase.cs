3d99bcd5fa2f2d8c4e496a049e29c2d06b197833
Interial <intromalek@gmail.com>
2018-05-03 22:11:01 +0200

    Add files via upload

---
+++ ConsoleApplication2/LogBase.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Net;
+using System.Xml;
+using Microsoft.Win32;
+
+namespace ConsoleApplication2
+{
+   public abstract class LogBase
+   {
+       protected readonly object lockObj = new object();
+        public abstract void Log(string message);
+
+   }
+
+    #region FileLogger
+
+    
+
+    
+    public class FileLogger : LogBase
+    {
+        public string filePath = @"C:\Intel\Logs\Intel.txt";
+        public override void Log(string message)
+        {
+            lock (lockObj)
+            {
+                using (StreamWriter streamWriter = new StreamWriter(filePath))
+                {
+                    streamWriter.WriteLine(message);
+                    streamWriter.Close();
+                }
+            }
+        }
+    }
+    #endregion
+    #region DBLogger
+    public class DBLogger : LogBase
+    {
+        string connectionString = string.Empty;
+        public override void Log(string message)
+        {
+            lock (lockObj)
+            {
+                
+
+
+            }
+            
+        }
+    }
+    #endregion
+    #region EventLogger
+    public class EventLogger : LogBase
+    {
+        public override void Log(string message)
+        {
+            lock (lockObj)
+            {
+                EventLog eventLog = new EventLog("");
+                eventLog.Source = "IDGEventLog";
+                eventLog.WriteEntry(message);
+            }
+        }
+    }
+    #endregion
+
+}
---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            LogHelper.Log(LogHelper.LogTarget.File, "Hello Maciek");
+            
+        }
+    }
+}
